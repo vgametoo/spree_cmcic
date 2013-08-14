c75e1af1d3d1eae670e12d9481b8e10d31e6a2f1
Vincent Charlet <pro.charlet@orange.fr>
2013-08-14 17:54:29 +0200

    Try redirect

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -9,18 +9,6 @@ module Spree
       payment_method = Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
       return unless payment_method.kind_of?(Spree::BillingIntegration::Cmcic)
 
-      update_params = object_params.dup
-      update_params.delete(:payments_attributes)
-      if @order.update_attributes(update_params)
-        fire_event('spree.checkout.update')
-        render :edit and return unless apply_coupon_code
-      end
-
-      load_order
-      if not @order.errors.empty?
-         render :edit and return
-      end
-
       redirect_to "https://paiement.creditmutuel.fr/" and return
     end
 
