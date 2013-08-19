ab8d9831155485878f73166a74a47917e1098a84
Vincent Charlet <pro.charlet@orange.fr>
2013-08-19 17:31:00 +0200

    Change route

--- app/models/spree/billing_integration/cmcic.rb
+++ app/models/spree/billing_integration/cmcic.rb
@@ -2,6 +2,6 @@ class Spree::BillingIntegration::Cmcic < Spree::BillingIntegration
 
   def redirect_url(order)
     
-    spree_cmcic_url(self.id, order)
+    spree_cmcic_order_checkout_url(self.id, order)
   end  
 end
